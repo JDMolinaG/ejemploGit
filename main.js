bc6c6f6a3484974e4afed94fa2ad8db4d9916c47
Juan David Molina <jdmg1027@gmail.com>
2021-02-20 12:25:26 -0500

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// se declaran las variables en cero
+let bello,sabaneta,sanJavier,total = 0;
+
+// esta variable va a recibir el nombre de la estacion
+let estacion
+
+// se compara la estacion con el nombre que se ingreso por consola
+if(estacion == 'bello'){
+    bello++
+}else if(estacion == 'sabaneta'){
+    sabaneta++
+}else if(estacion == 'san javier'){
+    sanJavier++
+}
+
+// se imprime el total de las estaciones
+console.log(total = bello + sabaneta + sanJavier)
+
+console.log(bello)
+console.log(sabaneta)
+console.log(sanJavier)
